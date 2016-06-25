a977547c9ade364ed3a9b044576cbe99439f65d0
ayyoob <ayyoobhamza@gmail.com>
2016-06-26 05:21:01 +0530

    added profile name to the startup script

--- local-cluster/clusterme.sh
+++ local-cluster/clusterme.sh
@@ -4,25 +4,25 @@ unzip wso2iots-1.0.0-SNAPSHOT.zip
 
 #create http-gateway setup
 cp -r wso2iots-1.0.0-SNAPSHOT http-gateway
-ditto -V config/http-gateway http-gateway
+rsync --recursive config/http-gateway/ http-gateway/
 
 
 #create mqtt-gateway setup
 cp -r wso2iots-1.0.0-SNAPSHOT mqtt-gateway
-ditto -V config/mqtt-gateway mqtt-gateway
+rsync --recursive config/mqtt-gateway/ mqtt-gateway/
 
 
 #create das setup
 cp -r wso2iots-1.0.0-SNAPSHOT das
-ditto -V config/das das
+rsync --recursive config/das/ das/
 
 #create device-key-manager setup
 cp -r wso2iots-1.0.0-SNAPSHOT device-key-manager
-ditto -V config/device-key-manager device-key-manager
+rsync --recursive config/device-key-manager/ device-key-manager/
 
 #create device-type publisher-manager setup
 cp -r wso2iots-1.0.0-SNAPSHOT devicetype-publisher
-ditto -V config/devicetype-publisher devicetype-publisher
+rsync --recursive config/devicetype-publisher/ devicetype-publisher/
 
 
 #Deploy Samples.
